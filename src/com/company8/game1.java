d88103c6daa10f2a4cfca08fabb312a9270cac1d
PC <2578489939@qq.com>
2019-06-03 11:09:10 +0800

    test

--- src/com/company8/game1.java
+++ src/com/company8/game1.java
@@ -6,10 +6,23 @@ public class game1 {
         char []a=new char[5];
         boolean[] flags=new boolean[letters.length];//默认值为false
         //随机选出字母
-        int num =(int)(Math.random()*letters.length);
+        /*int num =(int)(Math.random()*letters.length);
         for(int i=0;i<a.length;i++) {
             a[i]=letters[num];
+        }*/
+        int num;
+        for(int i=0;i<a.length;i++) {
+            do {
+                num = (int) (Math.random() * letters.length);
+            } while (flags[num]);
+            a[i]=letters[num];
+            flags[num]=true;
         }
         return a;
+    }
+    public static void main(String[] args){
+       int b=0+10;
+       System.out.println(b);
+       System.out.println(rnumber());
     }
 }
